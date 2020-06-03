1d7c640992bd3f2e3b03f2814b4d0910abfc8f98
dylanm2094 <59807851+dylanm2094@users.noreply.github.com>
2020-06-03 10:07:49 -0400

    base HW complete

---
+++ assets/js/app.js
@@ -0,0 +1,101 @@
+// @TODO: YOUR CODE HERE!
+var svgWidth = 700;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("../../assets/data/data.csv").then(function(data) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    data.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.smokes = +data.smokes;
+    });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(data, d => d.poverty), d3.max(data, d => d.poverty)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([d3.min(data, d => d.smokes), d3.max(data, d => d.smokes)])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.smokes))
+    .attr("r", "10")
+    .attr("fill", "lightblue")
+    .attr("opacity", "1");
+
+    var text=chartGroup.append("g").selectAll("text")
+                    .data(data)
+                    .enter()
+                    .append("text")
+                    .attr("x", d => xLinearScale(d.poverty))
+                    .attr("y", d => yLinearScale(d.smokes))
+                    .attr("font-family",  "Courier")
+                    .attr("fill", "black")
+                    .style("opacity", "0.8")
+                    .attr("font-size", "0.8em")
+                    .attr("text-anchor",  "middle")
+                    .text(function(d) {
+                        return d.abbr
+                     });
+
+    // Create axes labels
+    chartGroup.append("text")
+      // NOTE: Rotated Y axis label
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Smokers (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Poverty Rate (%)");
+  }).catch(function(error) {
+    console.log(error);
+  });
